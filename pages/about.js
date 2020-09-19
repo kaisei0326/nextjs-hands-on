70f6accd1060e7aa0e13da6b758c4f286db14c8b
kaisei0326 <kaisei0326@gmail.com>
2020-09-20 04:02:18 +0900

    Add _app _document _theme AND Fix about

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
+import Button from '@material-ui/core/Button';
 
 const About = () => {
-    return <>
-        <div>about page</div>
-        <Link href='/'>Home</Link>
-    </>
+    return (
+        <>
+            <div>about page</div>
+            <Link href="/">Home</Link>
+
+            <Button variant="contained" color="primary">
+                Hello!
+            </Button>
+        </>
+    );
 };
 
 export default About;
